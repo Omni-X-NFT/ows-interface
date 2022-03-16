ad05ded96cc129ed5fccd5845168717a5189146b
exakoss <danielkossman@yahoo.com>
2022-03-15 21:09:00 -0400

    feat: ethers and connect to MM in mint

--- pages/mint/index.tsx
+++ pages/mint/index.tsx
@@ -1,7 +1,21 @@
 import MainNav from '../../components/MainNav'
 import Head from 'next/head'
+import { ethers } from 'ethers'
+import { useEffect } from 'react'
 
 export default function Mint() {
+
+  useEffect(() => {
+    const connectToMetamask = async () => {
+      const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+      // Prompt user for account connections
+      await provider.send('eth_requestAccounts', [])
+      const signer = provider.getSigner()
+      console.log('Account:', await signer.getAddress())
+    }
+    connectToMetamask()
+  },[])
+  
   return(
     <div className='h-screen w-screen flex items-center justify-center bg-black'>
       <Head>
