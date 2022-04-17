f8f7d0bccfd62b67a8e6b56faae05ff2bf76b357
Ono Sakia <luckyonosakia@gmail.com>
2022-04-17 12:32:39 -0700

    alpha version

---
+++ components/Footer.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import social1 from '../static/social/social-1.svg'
+import social2 from '../static/social/social-2.svg'
+import social3 from '../static/social/social-3.svg'
+import social4 from '../static/social/social-4.svg'
+
+import logo from '../static/logo/omniverse-logo.svg'
+
+const Footer:React.FC = () => {
+  return(
+    <div className='footer flex justify-between h-[80px] py-[5px] px-5'>
+      <Link href={'/'}>
+        <div className='logo flex items-center cursor-pointer gap-[20px]'>
+          <Image src={logo} className='w-[70px]' />
+          <p className='text-[30px] leading-[50px]'>Omniverse </p>
+        </div>
+      </Link>
+      <ul className='gap-[30px] lg:flex items-center hidden'>
+        <li>
+          <Link href={'/'}>
+            <Image src={social1} className='cursor-pointer'>
+            </Image>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'}>
+            <Image src={social2} className='cursor-pointer'>
+            </Image>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'}>
+            <Image src={social3} className='cursor-pointer'>
+            </Image>
+          </Link>
+        </li>
+        <li>
+          <Link href={'/'}>
+            <Image src={social4} className='cursor-pointer'>
+            </Image>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,66 +2,183 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 import MainNav from '../components/MainNav'
-import SideNav from '../components/SideNav'
+import Footer from '../components/Footer'
 import Image from 'next/image'
 import omniLogo from '../static/omniverseLogoWhite.png'
 
 export default function Home() {
 
   const router = useRouter()
-  
 
   return (
-    <div className='h-screen w-screen flex items-center justify-center bg-black'>
+    <div className='w-full main'>
       <Head>
         <title>Omniverse DAO</title>
         <meta name='description' content='A homepage for Omniverse DAO'/>
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <MainNav/>
-      <div className='tabletcorner'>
-        <div className='tablet'>
-          <SideNav/>
-          <div className='main-grid'>
-            <div className='grid-header'>
-              welcome, anon, to the
+      <div className='w-full h-screen flex flex-col justify-center items-center'>
+        <p className='m-0 text-[20px] leading-[23px]'>ENTER THE</p>
+        <p className='m-0 text-[40px] md:text-[100px] leading-[120px]'>Omniverse</p>
+        <button className='bg-white text-black w-[231px] h-[61px] mt-[23px]'>Explore</button>
+      </div>
+      <div className='w-full items-center flex-col pt-[150px]'>
+        <div className='flex flex-col lg:flex-row w-full lg:w-5/6 m-auto pb-20'>
+          <div className='lg:w-2/5 w-full px-10 py-5'>
+            <p className='m-0 text-[50px] leading-[80px] text-center lg:text-right'>OVERVIEW</p>
+          </div>
+          <div className='lg:w-3/5 w-full lg:px-0 px-4 lg:py-14 py-0'>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>The Omniverse is an ecosystem of media, tools, and protocols unified by their development and interoperability via Layer Zero technology.</p>
+            <br/>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>Built by the Omniverse DAO with an emphasis on superior user experience.</p>
+          </div>
+        </div>
+        <div className='flex flex-col lg:flex-row w-full lg:w-5/6 m-auto pb-20'>
+          <div className='lg:w-2/5 w-full px-10 py-5'>
+            <p className='m-0 text-[50px] leading-[80px] text-center lg:text-right'>VISION</p>
+          </div>
+          <div className='lg:w-3/5 w-full lg:px-0 px-4 lg:py-14 py-0'>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>Creating a revolutionary game on groundbreaking interoperable blockchain technology will lead to us to developing a host of follow on products. There are no tools for this era of DAO run communities operating on multiple blockchains.</p>
+            <br/>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>With Omni X, we seek to change this. The first gaming DAO tool we are building is a multi-chain NFT marketplace framework. The Omni Wars game marketplace will be built on this framework, but we seek to open up this development to any other blockchain game looking to quickly bootstrap their own unique game specific NFT marketplace.</p>
+          </div>
+        </div>
+      </div>
+      <div className='w-full items-center flex-col lg:pt-[150px] pt-[80px]'>
+        <div className='flex flex-col lg:flex-row w-full lg:w-5/6 m-auto lg:pb-[180px] pb-20'>
+          <div className='lg:w-2/5 w-full px-10 py-5 flex flex-col lg:items-end items-center'>
+            <div className='w-[200px]'>
+              <img src='../static/logo/omniverse-logo.svg' />
+              <p className='m-0 mt-8 text-[30px] leading-[35px] text-center'>OmniX</p>
             </div>
-            <div className='flex'>
-              <Image src={omniLogo} width={200} height={200}/>
-              <span className='grid-verse'>MNIVERSE</span>
+          </div>
+          <div className='lg:w-3/5 w-full lg:px-0 px-4 lg:py-14 py-0'>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>A product suite providing tools for creating Omnichain tokens, Governance platforms, Treasuries and NFT marketplaces. </p>
+            <br/>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>Leveraging the power of sourcing both financial and social capital from multiple chains.</p>
+          </div>
+        </div>
+        <div className='flex flex-col lg:flex-row w-full lg:w-5/6 m-auto pb-20'>
+          <div className='lg:w-2/5 w-full px-10 py-5 flex flex-col lg:items-end items-center'>
+            <div className='w-[200px]'>
+              <img src='../static/logo/omniwar-logo.svg' />
+              <p className='m-0 mt-8 text-[30px] leading-[35px] text-center'>OmniWars</p>
             </div>
-            <div className='flex mt-5'>
-              <div className='left-bold'>
-                <i>what is it?</i>
-              </div>  
-              <div className='right-text self-end'>
-                The Omniverse is an ecosystem of media, tools, and protocols unified by their development and interoperability via Layer Zero technology and a emphasis on a superior UI/UX experience.
-                <p>This ecosystem of products is run by the Omniverse DAO.</p>
-              </div>
+          </div>
+          <div className='lg:w-3/5 w-full lg:px-0 px-4 lg:py-14 py-0'>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>An omnichain compete-to-earn (C2E) strategy game developed by Omniverse DAO and built on the LayerZero protocol.</p>
+            <br/>
+            <p className='text-[20px] m-0 leading-[23px] text-justify lg:text-left'>The C2E model puts maximum emphasis on the team work and strategic development rather than endless grind and randomized rewards.</p>
+          </div>
+        </div>
+      </div>
+      <div className='h-[380px] lg:block hidden'>
+      </div>
+      <div className='flex flex-col items-center'>
+        <p className='lg:text-[50px] text-[25px] lg:leading-[120px] leading-[60px] m-0 mb-[30px] uppercase text-white color text-center'>OMNichain Powered by layerZERO</p>
+        <div className='flex lg:flex-row flex-col gap-[40px] mb-[40px]'>
+          <img src='../static/logo/ethereum-eth-logo-1.svg' />
+          <img src='../static/logo/dbanner1_copy_1.svg' />
+          <img src='../static/logo/dbanner1_copy_2_1.svg' />
+          <img src='../static/logo/dbanner1_copy_4_1.svg' />
+        </div>
+        <div className='flex lg:flex-row flex-col gap-[40px]'>
+          <img src='../static/logo/JtpX95Rt_400x400-1.svg' />
+          <img src='../static/logo/dbanner1_copy_3_1.svg' />
+          <img src='../static/logo/fantom-ftm-logo-1.svg' />
+        </div>
+      </div>
+      <div className='h-[200px] lg:block hidden'>
+      </div>
+      <div className='lg:flex hidden flex-col items-center'>
+        <p className='lg:text-[50px] text-[25px] lg:leading-[120px] leading-[60px] m-0 text-white color text-center'>ROADMAP</p>
+        <div className='lg:mt-[45px] mt-[20px] w-9/12 mx-auto'>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-white.svg' />
+              <img src='../static/road_map/right-white.svg' />
+              <img className='absolute bottom-[5px] left-[51%]' src='../static/road_map/line-right.svg' />
             </div>
-            <div className='flex mt-5'>
-              <div className='left-bold'>
-                <i>what are we building?</i>
-              </div>  
-              <div className='right-text self-end'>
-                Our flagship product and proof of concept for all our developed technologies and planned products is a compete to earn (C2E) game - Omni Wars.
-
-                <p>Creating a revolutionary game on groundbreaking interoperable blockchain technology will lead to us to developing a host of follow on products. There are no tools for this era of DAO run communities operating on multiple blockchains.</p>
-
-                <p>With Omni X, we seek to change this. The first gaming DAO tool we are building is a multi-chain NFT marketplace framework. The Omni Wars game marketplace will be built on this framework, but we seek to open up this development to any other blockchain game looking to quickly bootstrap their own unique game specific NFT marketplace.</p>
-              </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 bottom-0'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] text-justify lg:text-right'>Capital Raise</p>
+              <p className='text-[20px] m-0 leading-[30px] text-justify lg:text-right'>Expanded team and added core contributors to collaborate on development.</p>
             </div>
-            <div className='flex mt-5'>
-              <div className='left-bold'>
-                <i>tell me more!</i>
-              </div>  
-              <div className='right-text self-end'>
-                There are so many plans ahead - read on in our Roadmap in this section to get a sense of our timeline. Select the Omni Wars and Omni X tabs to learn more about these enterprises.
-              </div>
+            <div className='lg:w-[530px] w-4/5  absolute right-0 top-[-60px]'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] text-justify lg:text-left'>Community & Core team Growth</p>
+              <p className='text-[20px] m-0 leading-[30px] text-justify lg:text-left'>Launched Socials and github launched to establish, foster and grow community.</p>
+              <p className='text-[30px] m-0 mr-[100px] leading-[50px] text-right'>Mar 2022</p>
+            </div>
+          </div>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-white.svg' />
+              <img src='../static/road_map/right-white.svg' />
+              <img className='absolute bottom-[5px] right-[51%]' src='../static/road_map/line-left.svg' />
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 top-[20%]'>
+              <p className='text-[30px] m-0 ml-[60px] uppercase leading-[50px] text-left'>APR 2022</p>
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute right-0 bottom-0'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] lg:text-left'>GREG</p>
+              <p className='text-[20px] m-0 leading-[30px] lg:text-left'>Launch Website showcasing interchain nft asset transfer. Genesis omniverse greg omni-nft colection. </p>
+            </div>
+          </div>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-grey.svg' />
+              <img src='../static/road_map/right-grey.svg' />
+              <img className='absolute bottom-[5px] left-[51%]' src='../static/road_map/line-right.svg' />
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute right-0 top-[20%]'>
+              <p className='text-[30px] m-0 mr-[60px] leading-[50px] text-right'>Mar 2022</p>
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 bottom-0'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] lg:text-right'>Omni X</p>
+              <p className='text-[20px] m-0 leading-[30px] lg:text-right'>Alpha NFT marketplace infrastructure release</p>
+            </div>
+          </div>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-grey.svg' />
+              <img src='../static/road_map/right-grey.svg' />
+              <img className='absolute bottom-[5px] right-[51%]' src='../static/road_map/line-left.svg' />
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 top-[20%]'>
+              <p className='text-[30px] m-0 ml-[60px] uppercase leading-[50px] text-left'>JUNE 2022</p>
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute right-0 bottom-0'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] lg:text-left'>faction nfts</p>
+              <p className='text-[20px] m-0 leading-[30px] lg:text-left'>NFT PFP Collections: faction specific GENESIS collections. Engagement Competitions: rewards for world building, fan art, and lore production.</p>
+            </div>
+          </div>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-grey.svg' />
+              <img src='../static/road_map/right-grey.svg' />
+              {/*<img className='absolute bottom-[2px] left-[78px]' src='../static/road_map/line-right.svg' />*/}
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute right-0 top-[20%]'>
+              <p className='text-[30px] m-0 mr-[60px] leading-[50px] text-right'>Q3 2022</p>
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 bottom-0'>
+              <p className='text-[30px] m-0 uppercase leading-[50px] lg:text-right'>Omni Wars</p>
+              <p className='text-[20px] m-0 leading-[30px] lg:text-right'>Alpha gameplay launch</p>
+            </div>
+          </div>
+          <div className='relative w-full h-[240px]'>
+            <div className='lg:flex hidden justify-center left-[46%]'>
+              <img src='../static/road_map/left-grey.svg' />
+              <img src='../static/road_map/right-grey.svg' />
+              <img className='absolute bottom-[5px] right-[51%]' src='../static/road_map/line-left.svg' />
+            </div>
+            <div className='lg:w-[530px] w-4/5  absolute left-0 top-[20%]'>
+              <p className='text-[30px] m-0 ml-[60px] uppercase leading-[50px] text-left'>Q4 2022</p>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
