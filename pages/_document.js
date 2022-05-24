a607914f14c01222581253c43fcf25b7d44d3c12
socr102 <normanburtonfree@gmail.com>
2022-05-24 18:21:41 +0900

    fix space issue when click explorer button

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,7 @@ class MyDocument extends Document {
           <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet" />
           
         </Head>
+
         <body>
           <Main />
           <NextScript />
