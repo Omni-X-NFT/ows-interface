80048823ea39be6bd5586c9d63745a8f6c0cd18c
exakoss <danielkossman@yahoo.com>
2022-08-05 01:09:13 -0400

    fix: vision and overview content

--- pages/index.tsx
+++ pages/index.tsx
@@ -188,13 +188,13 @@ const Home: NextPage = () => {
               <div className={styles.ContentWrap}  >
               <div className={styles.contentBody}>
                   <h3 id="version_head">Vision</h3>
-                  <p id="version_text1">We at Omni X seek to enable the first true metaverse - where virtual worlds, users, and protocols deployed on  any blockchain are able to freely communicate and exchange value in a frictionless manner.</p>
-                  <p id="version_text2">We are driven to create products that unlock the full potential of artists and communities to interact and prosper. Our emphasis on intuitive user experience, empowered by LayerZero technology, innovative tooling and social interactivity combines for a powerful and novel experience unlike any created before.</p>
+                  <p id="version_text1">A multichain world requires omnichain solutions. Artists, builders, collectors and gamers should be able to transfer and trade assets from varying ecosystems without leaving a single user interface. Frictionless communication and exchange of value are the cornerstones of an implied ideal web3 experience, but currently the liquidity and communities get dispersed throughout multiple EVM and non-EVM chains .</p>
+                  <p id="version_text2">We seek to change that. Our products unlock the full potential of creators and communities to interact and prosper. The emphasis on intuitive user experience, empowered by LayerZero technology, innovative tooling and social interactivity combines for a powerful and novel experience unlike any created before.</p>
                 </div>
 
                 <div className={styles.contentBody}>
                   <h3 id="overview_head">Overview</h3>
-                  <p id="overview_text1">Omni X is our flagship endeavor into this initiative. Omni X is the first omnichain NFT platform. Beyond a simple marketplace, Omni X connects communities, creators, and enthusiasts on a level never before seen. Launch omnichain NFTs and gain access to unparalleled liquidity that allows users to buy and sell NFTs from any blockchain to any other blockchain.</p>
+                  <p id="overview_text1">Omni X is the first natively omnichain NFT platform. Beyond a simple marketplace, Omni X connects communities, creators, and enthusiasts on a level never before seen. Launch omnichain NFTs and gain access to unparalleled liquidity that allows users to buy and sell NFTs from any blockchain to any other blockchain.</p>
                   <p id="overview_text2">As our project grows, we will continue to build and push the boundaries of what blockchain technology allows, always with the ultimate goal in mind of creating the best possible experience for every type of user.</p>
                 </div>
 
